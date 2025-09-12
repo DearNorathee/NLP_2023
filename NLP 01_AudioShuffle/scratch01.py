663dbac6fb4d11728d69374b0ad3d9c993e631f9
NoratheeLert <noratee5@hotmail.com>
2025-09-12 12:01:22 +0700

    Added Template and German audio shuffle

--- NLP 01_AudioShuffle/scratch01.py
+++ NLP 01_AudioShuffle/scratch01.py
@@ -1,12 +1,12 @@
 import ffmpeg
 import os
 import wave
-def temp_fun(x):
-    return x
+import os_toolkit as ost
+import pandas as pd
 
-from moviepy.editor import VideoFileClip
-folder_path = r"C:\Users\Heng2020\OneDrive\Python NLP\OutputData"
 
-input_full = r"C:\C_Video_Python\Learn French\100 questions et réponses en français A1 à C1\Question and Answer Part_YoutubeSub"
-output_name = 'PT01.wav'
+
+input_path = r"C:\C_Video_Python\Learn German\Learn German with Nico\Splitted Audio\Learn German with Nico_A1"
+
+paths_df = pd.DataFrame(ost.get_full_filename(input_path))
 
