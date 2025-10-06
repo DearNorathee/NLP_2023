306e71feb66509beb65820fc8bb4549214285149
NoratheeLert <noratee5@hotmail.com>
2025-10-06 14:37:33 +0700

    -

--- NLP 01_AudioShuffle/scratch01.py
+++ NLP 01_AudioShuffle/scratch01.py
@@ -8,5 +8,7 @@ import pandas as pd
 
 input_path = r"C:\C_Video_Python\Learn German\Learn German with Nico\Splitted Audio\Learn German with Nico_A1"
 
-paths_df = pd.DataFrame(ost.get_full_filename(input_path))
+paths_df = pd.DataFrame(ost.get_full_filename(input_path), columns = ['audio_path'])
+
+paths_df_step2 = paths_df.loc[~paths_df['audio_path'].str.contains('_ori')] 
 
